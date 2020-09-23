70558609bf3a0e9ac2fd4c0219e052f19dfb49a8
djbittner <djbittner@dmacc.edu>
2020-09-22 19:58:58 -0500

    3rd test pass

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -11,6 +11,23 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(3.38, coupon.calculate_price(7.15, 10, 15), 2)
         self.assertAlmostEqual(.78, coupon.calculate_price(3.90, 10, 20), 2)
 
+    def test_price_under_between_ten_thirty(self):
+        self.assertAlmostEqual(11.20, coupon.calculate_price(10.50, 5, 10), 2)
+        self.assertAlmostEqual(16.45, coupon.calculate_price(16.65, 5, 15), 2)
+        self.assertAlmostEqual(23, coupon.calculate_price(22.75, 5, 20), 2)
+        self.assertAlmostEqual(26.22, coupon.calculate_price(29.15, 10, 10), 2)
+        self.assertAlmostEqual(14.96, coupon.calculate_price(20, 10, 15), 2)
+        self.assertAlmostEqual(20.16, coupon.calculate_price(24.40, 10, 20), 2)
+
+    def test_price_under_between_thirty_fifty(self):
+        self.assertAlmostEqual(36.57, coupon.calculate_price(35, 5, 10), 2)
+        self.assertAlmostEqual(47.45, coupon.calculate_price(44.40, 5, 15), 2)
+        self.assertAlmostEqual(48.41, coupon.calculate_price(47.99, 5, 20), 2)
+        self.assertAlmostEqual(38.17, coupon.calculate_price(41.68, 10, 10), 2)
+        self.assertAlmostEqual(27.01, coupon.calculate_price(31.15, 10, 15), 2)
+        self.assertAlmostEqual(33.29, coupon.calculate_price(39.88, 10, 20), 2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
